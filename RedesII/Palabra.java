fa444ec5932ba9e9bab4e59e6af5d54328846a5c
Jimmyzaurio <wizard.jsp@gmail.com>
2016-03-28 02:57:26 -0600

    Update Palabra.java

--- RedesII/Palabra.java
+++ RedesII/Palabra.java
@@ -2,23 +2,17 @@ import java.io.*;
 import java.util.*;
 
 public class Palabra {
-	String[] A = {"OSO", "COTORRO", "PERICO", "ANA", "RECONOCER","SOMETEMOS", "LUNA", 
-	"RODAR", "ORO", "AVE", "MOCOSO", "HADA", "MOTOR", "ROTO", "TROPA", "AMOR", "AROMA", 
-	"VENUS", "ALMA", "BROMA", "TUMBAS", "LETRAS", "REDES"};
+	String[] a;
 
 	Set<String> damePalabras() {
-		Random ra = new Random();
-		int tam = ra.nextInt(A.length - 9) + 9;
+		Archivo arc = new Archivo();
+		a = arc.leeLista();
+		Random ra = new Random();		
+		int tam = ra.nextInt(14) + 15;
 
 		Set<String> S = new HashSet<String>();
-		for (;S.size() < tam;) {
-			if (ra.nextInt(2) % 2 == 0)
-				S.add( new StringBuilder(A[ra.nextInt(tam)]).
-					reverse().toString()
-					);
-			else
-				S.add(A[ra.nextInt(tam)]);
-		}
+		for (;S.size() < tam;) 
+			S.add(a[ra.nextInt(tam)]);		
 		return S;
 	}
 }
