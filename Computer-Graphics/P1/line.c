194e5895e1c21a744256ede7e80167affe92b75d
Jimmyzaurio <jsp.saucedo@gmail.com>
2016-05-29 15:09:20 -0500

    Update line.c

--- Computer-Graphics/P1/line.c
+++ Computer-Graphics/P1/line.c
@@ -9,7 +9,8 @@ int aleatorio() { return rand()%255; }
 void line(int x1, int y1, int x2, int y2) {
 	double m = abs((y1 - y2)/(x1 - x2)); // Sacamos el valor absoluto para no que no intente guardar en una dirección negativa
 	int y = y1;
-	for (int x = x1; x <= x2; ++x) {
+	int x;
+	for (x = x1; x <= x2; ++x) {
 		y += m;
 		raster[y][x][0] = 0;
 		raster[y][x][1] = 0;
@@ -21,8 +22,9 @@ void paint() {
 	printf("P3\n");
   	printf("1920 1080\n");
   	printf("255\n");
-	for (int i = 0; i < 1080; ++i)
-		for (int j = 0; j < 1920; ++j)
+  	int i, j;
+	for (i = 0; i < 1080; ++i)
+		for (j = 0; j < 1920; ++j)
 			printf(" %d %d %d ", raster[i][j][0], raster[i][j][1], raster[i][j][2]);
     printf("\n");
 }
@@ -31,14 +33,15 @@ int main() {
 	int x1, x2, y1, y2;
 	srand(time(NULL));
 
-	for (int i = 0; i < 1080; ++i) {
-		for (int j = 0; j < 1920; ++j) {
+	int i, j;
+	for (i = 0; i < 1080; ++i) {
+		for (j = 0; j < 1920; ++j) {
 			raster[i][j][0] = 255;
 			raster[i][j][1] = 255;
 			raster[i][j][2] = 255;
 		}
 	}
-	for (int i = 0; i < 1000; ++i) {
+	for (i = 0; i < 1000; ++i) {
 		x1 = aleatorio()%1920;
 		y1 = aleatorio()%1080;
 		x2 = ((aleatorio()%1920) + x1 + 1)%1920;
@@ -46,4 +49,5 @@ int main() {
 		line(x1, y1, x2, y2);
 	}
 	paint();
+	return 0;
 }
